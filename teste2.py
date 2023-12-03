450d936a278b31bdbf53cef3e8ad25d311d80e41
rafanog0 <rafanogob@gmail.com>
2023-12-03 16:58:57 -0300

    Adicionado 'requirements.txt'

---
+++ teste2.py
@@ -0,0 +1,47 @@
+from pywebio.input import *
+from pywebio.output import put_text
+import json
+import random
+from pymongo import MongoClient
+from pywebio.output import put_text, popup
+
+
+# Conexão com o MongoDB
+client = MongoClient("mongodb://localhost:27017/")
+db = client["meu_banco_de_dados"]
+colecao = db["personagens"]
+
+def collect_character_data():
+        # Solicitar dados básicos do personagem
+    data = input_group("Dados do Personagem Fictício", [
+        input("Nome", name="nome"),
+        select("Vocação", ['Guerreiro', 'Mago', 'Arqueiro', 'Healer'], name="vocacao"),
+        radio("Sexo", options=['Masculino', 'Feminino', 'Outro'], name="sexo"),
+        textarea("Contexto/História", name="contexto"),
+        input("Idade", type="number", name="idade"),
+        textarea("Habilidades", name="habilidades")
+    ])
+
+    # Gerar atributos automaticamente
+    atributos = {'Inteligência': 0, 'Destreza': 0, 'Força': 0, 'Carisma': 0}
+    total_atributos = 36
+
+    while total_atributos > 0:
+        for atributo in atributos:
+            if total_atributos > 0:
+                ponto = random.randint(0, total_atributos)
+                atributos[atributo] += ponto
+                total_atributos -= ponto
+
+    data['atributos'] = atributos
+
+    # Converter os dados para formato JSON
+    json_data = json.dumps(data, indent=4)
+    put_text("Dados do personagem em formato JSON:\n" + json_data)
+
+    # Inserir os dados no MongoDB
+    colecao.insert_one(data)
+    put_text("Dados inseridos no MongoDB com sucesso.")
+
+if __name__ == '__main__':
+    collect_character_data()
