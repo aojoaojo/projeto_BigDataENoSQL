55877d5129caa450167e88c7400990cb6a58184c
rafanog0 <rafanogob@gmail.com>
2023-12-03 16:48:37 -0300

    first

--- app.py
+++ app.py
@@ -1,15 +1,38 @@
-from pywebio.input import *
+from pywebio.input import input, select, radio, textarea, input_group
+from pywebio.output import put_text, popup
+from pywebio import start_server
+import json
 
-    
-nome = input("Digite o nome do seu personagem: ", type=TEXT)
-classe = select("Escolha a classe do seu personagem: ", ['Guerreiro', 'Mago', 'Arqueiro'])
-genero = checkbox("Escolha o seu gênero:", options=["Masculino",'Feminino','Corno','Doido'])
-# radio("Select any one", options=['1', '2', '3'])
-# textarea('Text Area', rows=3, placeholder='Multiple line text input')
+def collect_character_data():
+    while True:
+        # Solicitar dados do personagem
+        data = input_group("Dados do Personagem Fictício", [
+            input("Nome", name="nome"),
+            select("Vocação", ['Guerreiro', 'Mago', 'Arqueiro', 'Healer'], name="vocacao"),
+            radio("Sexo", options=['Masculino', 'Feminino', 'Outro'], name="sexo"),
+            textarea("Contexto/História", name="contexto"),
+            input("Idade", type="number", name="idade"),
+            textarea("Habilidades", name="habilidades"),
+            input("Inteligência", type="number", name="inteligencia", min_value=0, max_value=36),
+            input("Destreza ", type="number", name="destreza", min_value=0, max_value=36),
+            input("Força ", type="number", name="forca", min_value=0, max_value=36),
+            input("Carisma ", type="number", name="carisma", min_value=0, max_value=36)
+            
+        ])
 
+        # Calcular a soma dos atributos
+        total_atributos = data['inteligencia'] + data['destreza'] + data['forca'] + data['carisma']
+        
+        if total_atributos > 36:
+            popup("Erro", "A soma total dos atributos não pode exceder 36. Por favor, redistribua os pontos.")
+            continue
 
+        break
 
-with open('personagem.txt', 'w') as f:
-    code = f.write(f'Nome: {nome}\nClasse: {classe}\nGenero: {genero}\n')
+    # Converter os dados para formato JSON
+    json_data = json.dumps(data, indent=4)
+    put_text("Dados do personagem em formato JSON:\n" + json_data)
 
-f.close()
+# Iniciar a coleta de dados
+if __name__ == '__main__':
+    start_server(collect_character_data, port=8080)
