59d29fcb0e7d76caaa45b454921232098dfa8f3c
Joao <aojoaojo@pop-os.localdomain>
2023-12-03 19:46:56 -0300

    implementada a funcao de mostrar as spells para a classe criada. Limpa a tela após mostrar os atributos por 4 segundos

--- app.py
+++ app.py
@@ -3,19 +3,30 @@ from pywebio.output import put_text
 import json
 import random
 from pymongo import MongoClient
-from pywebio.output import put_text, popup
-
+from pywebio.output import put_text, popup, put_markdown, clear
+import time
 
 # Conexão com o MongoDB
 client = MongoClient("mongodb://root:mongo@localhost:27017")
 db = client["DnD"]
 colecao = db["personagem"]
+dbSpell = client["DnD"]
+colecaoSpell = db["Spells"]
+
+
+def main():
+    data = collect_character_data()
+    converter_para_json(data)
+    consultar_spell(data)
+
+
+
 
 def collect_character_data():
         # Solicitar dados básicos do personagem
     data = input_group("Dados do Personagem", [
         input("Nome", name="nome"),
-        select("Classe", ['Guerreiro', 'Mago', 'Arqueiro', 'Healer'], name="vocacao"),
+        select("Classe", ['Artificer', 'Sorcerer', 'Wizard', 'Bard', 'Warlock', 'Druid', 'Cleric', 'Ranger', 'Paladin'], name="classe"),
         radio("Sexo", options=['Masculino', 'Feminino', 'Outro'], name="sexo"),
         textarea("Contexto/Historia", name="contexto"),
         input("Idade", type="number", name="idade"),
@@ -34,14 +45,42 @@ def collect_character_data():
                 total_atributos -= ponto
 
     data['atributos'] = atributos
+    
+    return data
 
+
+
+
+def converter_para_json(data):
     # Converter os dados para formato JSON
-    json_data = json.dumps(data, indent=4)
-    put_text("Dados do personagem em formato JSON:\n" + json_data)
-
+    # json_data = json.dumps(data, indent=4)
+    put_markdown("# Os seus atributos foram:\n")
+    for atrib in data['atributos']:
+        put_markdown('### ' + atrib + ': ' + str(data['atributos'][atrib]))
+    
     # Inserir os dados no MongoDB
     colecao.insert_one(data)
-    put_text("Dados inseridos no MongoDB com sucesso.")
+    time.sleep(1)
+    clear()
+    # put_text("Dados inseridos no MongoDB com sucesso.")
+
+
+
+
+
+def consultar_spell(data):
+    # Consultar spells no Mongodb
+    
+    query = {}
+    result = colecaoSpell.find(query)
+    
+    put_markdown('## Spells disponiveis para a sua classe: ')
+    for document in result:
+        if data['classe'] in document['classes']:
+            put_markdown('### ' + document['name'])
+            put_markdown('#### ' + document['description'])
+
+
 
 if __name__ == '__main__':
-    collect_character_data()
+    main()
