9b2a47ad5c64407cd3bfc9b6eafa34831d304b79
rafanog0 <rafanogob@gmail.com>
2023-12-03 16:57:05 -0300

    Conexão com mongoDB e atributos agora são aleatorios

--- app.py
+++ app.py
@@ -1,38 +1,47 @@
-from pywebio.input import input, select, radio, textarea, input_group
-from pywebio.output import put_text, popup
-from pywebio import start_server
+from pywebio.input import *
+from pywebio.output import put_text
 import json
+import random
+from pymongo import MongoClient
+from pywebio.output import put_text, popup
+
+
+# Conexão com o MongoDB
+client = MongoClient("mongodb://localhost:27017/")
+db = client["meu_banco_de_dados"]
+colecao = db["personagens"]
 
 def collect_character_data():
-    while True:
-        # Solicitar dados do personagem
-        data = input_group("Dados do Personagem Fictício", [
-            input("Nome", name="nome"),
-            select("Vocação", ['Guerreiro', 'Mago', 'Arqueiro', 'Healer'], name="vocacao"),
-            radio("Sexo", options=['Masculino', 'Feminino', 'Outro'], name="sexo"),
-            textarea("Contexto/História", name="contexto"),
-            input("Idade", type="number", name="idade"),
-            textarea("Habilidades", name="habilidades"),
-            input("Inteligência", type="number", name="inteligencia", min_value=0, max_value=36),
-            input("Destreza ", type="number", name="destreza", min_value=0, max_value=36),
-            input("Força ", type="number", name="forca", min_value=0, max_value=36),
-            input("Carisma ", type="number", name="carisma", min_value=0, max_value=36)
-            
-        ])
+        # Solicitar dados básicos do personagem
+    data = input_group("Dados do Personagem Fictício", [
+        input("Nome", name="nome"),
+        select("Vocação", ['Guerreiro', 'Mago', 'Arqueiro', 'Healer'], name="vocacao"),
+        radio("Sexo", options=['Masculino', 'Feminino', 'Outro'], name="sexo"),
+        textarea("Contexto/História", name="contexto"),
+        input("Idade", type="number", name="idade"),
+        textarea("Habilidades", name="habilidades")
+    ])
 
-        # Calcular a soma dos atributos
-        total_atributos = data['inteligencia'] + data['destreza'] + data['forca'] + data['carisma']
-        
-        if total_atributos > 36:
-            popup("Erro", "A soma total dos atributos não pode exceder 36. Por favor, redistribua os pontos.")
-            continue
+    # Gerar atributos automaticamente
+    atributos = {'Inteligência': 0, 'Destreza': 0, 'Força': 0, 'Carisma': 0}
+    total_atributos = 36
 
-        break
+    while total_atributos > 0:
+        for atributo in atributos:
+            if total_atributos > 0:
+                ponto = random.randint(0, total_atributos)
+                atributos[atributo] += ponto
+                total_atributos -= ponto
+
+    data['atributos'] = atributos
 
     # Converter os dados para formato JSON
     json_data = json.dumps(data, indent=4)
     put_text("Dados do personagem em formato JSON:\n" + json_data)
 
-# Iniciar a coleta de dados
+    # Inserir os dados no MongoDB
+    colecao.insert_one(data)
+    put_text("Dados inseridos no MongoDB com sucesso.")
+
 if __name__ == '__main__':
-    start_server(collect_character_data, port=8080)
+    collect_character_data()
